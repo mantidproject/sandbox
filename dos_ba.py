abfb5bff1f4576aae09f667d9ebf93e2bebd492d
Dan Nixon <dan@dan-nixon.com>
2015-08-06 11:57:59 +0000

    This was a test

--- dos_ba.py
+++
@@ -1,3 +0,0 @@
-ba = SimulatedDensityOfStates(CASTEPFile='/home/dan/Dropbox/sim/Croco.castep',
-                              PHONONFile='/home/dan/Dropbox/sim/Croco.phonon',
-                              SpectrumType='BondAnalysis')
