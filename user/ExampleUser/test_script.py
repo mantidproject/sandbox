4f7ad2885d8a5021c902a63334bd04ad90d8e70d
Alice Russell <alice.russell@stfc.ac.uk>
2019-10-28 13:30:05 +0000

    Test script

--- user/ExampleUser/test_script.py
+++ user/ExampleUser/test_script.py
@@ -1,3 +1,3 @@
-# This is a test script to test the functionality of the script repo in workbench fsdht
+# This is a test script to test the functionality of the script repo in workbench
 
 print("Hello World!")
